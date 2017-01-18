5a689160b06011ec30dc8d60895a0a1a87bb3fc5
k8517 <k8517@ICT-421-WS-05>
2017-01-18 13:56:09 +0200

    Uusi harjoitus lisätty

---
+++ Tuntiharjoitus2/Harjoitukset.cs
@@ -0,0 +1,46 @@
+﻿/* **************************************
+Tuntiharjoituksille pääohjelma.
+
+Luotu 18.1.2017
+
+Minttu Mäkäläinen K8517 @ JAMK
+************************************** */
+
+using JAMK.IT;                          // Helpottaa meidän luokkien löytämistä
+using System;
+
+namespace Tuntiharjoitukset
+{
+    class Harjoitukset
+    {
+        static void Main(string[] args)
+        {
+            TestaaHissi();            // Tehtävä 1
+
+        }
+
+        /**********************************************
+        *      Tehtävä 1 - Hissi-olion testausta      *
+        **********************************************/
+        static void TestaaHissi()
+        {
+            Hissi hissi = new Hissi();  // Luodaan Hissi-olio
+
+            while (true)
+            {
+                int kerros = 0;
+                Console.Write("Mikä kerros? (1-5) > ");
+                string rivi = Console.ReadLine();
+
+                bool tulos = int.TryParse(rivi, out kerros);        // out kerros --> toimitetaan luokkaohjelmalle
+
+                if (tulos)
+                {
+                    hissi.Kerros = kerros;
+                }
+
+                else break;
+            }
+        }
+    }
+}
---
+++ Tuntiharjoitus2/Hissi.cs
@@ -0,0 +1,44 @@
+﻿/* **************************************
+Harjoitellaan luokkien ja ominaisuuksien tekoa ja käyttöä.
+
+Luotu 18.1.2017
+
+Minttu Mäkäläinen K8517 @ JAMK
+************************************** */
+
+using System;
+
+namespace JAMK.IT
+{
+    public class Hissi
+    {
+        int kerros;
+        public int Kerros
+        {
+            get
+            {
+                return kerros;
+            }
+            set
+            {
+                kerros = value;
+                if (kerros < 1)
+                {
+                    Console.WriteLine("Kerros on liian pieni!");
+                    kerros = 1;
+                }
+
+                if (kerros > 5)
+                {
+                    Console.WriteLine("Kerros on liian iso!");
+                    kerros = 5;
+                }
+
+                if (kerros > 0 && kerros < 6)
+                {
+                    Console.WriteLine("Kerros on nyt " + kerros);
+                }
+            }
+        }
+    }
+}
