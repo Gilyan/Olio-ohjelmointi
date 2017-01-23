c4da636066fdc654154f57acb403f4b3768edb11
k8517 <k8517@ICT-421-WS-05>
2017-01-23 13:28:20 +0200

    Harjoituksen lisääminen

--- Tuntiharjoitukset3/Harjoitukset.cs
+++ Tuntiharjoitukset3/Harjoitukset.cs
@@ -8,54 +8,63 @@ Minttu Mäkäläinen K8517 @ JAMK
 
 using JAMK.IT;                          // Helpottaa meidän luokkien löytämistä
 using System;
+using System.Collections.Generic;
 
-namespace Tuntiharjoitukset
+namespace Harjoituksia
 {
     class Harjoitukset
     {
         static void Main(string[] args)
         {
-            //TestaaHissi();            // Tehtävä 1
-
+            TestaaRengas();             // Tehtävä 1
+            //TestaaKylmakaappi();        // Tehtävä 2
+            //TestaaNisakas();            // Tehtävä 3
+            //Testaadsafdsfds();          // Tehtävä 4
         }
 
         /**********************************************
-        *      Tehtävä 1 - Hissi-olion testausta      *
+        *      Tehtävä 1 - Rengas-olion testausta     *
         **********************************************/
-        static void TestaaHissi()
+        static void TestaaRengas()
         {
+            Rengas nokia = new Rengas("Nokia", "Hakkapeliitta", "205R16");
 
+            Auto auto = new Auto();
+
+            auto.Nimi = "Porsche";
+            auto.Malli = "911";
+
+            Console.WriteLine("Luotu uusi auto : {0} {1}", auto.Nimi, auto.Malli);
+            auto.LisaaRengas(nokia);
+            auto.LisaaRengas(nokia);
+            auto.LisaaRengas(nokia);
+            auto.LisaaRengas(nokia);
+
+            Console.WriteLine(auto.ToString());
         }
 
         /**********************************************
         *    Tehtävä 2 - Vahvistin-olion testausta    *
         **********************************************/
+        static void TestaaKylmakaappi()
+        {
 
+        }
 
         /**********************************************
         *  Tehtävä 3 - Henkilotiedot-olion testausta  *
         **********************************************/
+        static void TestaaNisakas()
+        {
 
+        }
 
         /**********************************************
         *   Tehtävä 4 - Kulkuneuvot-olion testausta   *
         **********************************************/
+        static void Testaadsafdsfds()
+        {
 
-
-        /**********************************************
-         *     Tehtävä 5 - Radio-olion testausta      *
-        **********************************************/
-
-
-        /**********************************************
-         *   Tehtävä 6 - Kirjahylly-olion testausta   *
-        **********************************************/
-
-
-        /**********************************************
-         *  Tehtävä 7 - Paivakirja-olion testausta    *
-        **********************************************/
-
-
+        }
     }
 }
---
+++ Tuntiharjoitukset3/Rengas.cs
@@ -0,0 +1,68 @@
+﻿/* **************************************
+Harjoitellaan koostamista.
+
+Luotu 23.1.2017
+
+Minttu Mäkäläinen K8517 @ JAMK
+************************************** */
+
+using System;
+using System.Collections.Generic;
+
+namespace JAMK.IT
+{
+    public class Rengas
+    {
+        public string Merkki { get; set; }
+        public string Tyyppi { get; set; }
+        public string Rengaskoko { get; set; }
+        public Rengas(string model, string type, string size)
+        {
+            Merkki = model;
+            Tyyppi = type;
+            Rengaskoko = size;
+        }
+        public override string ToString()
+        {
+            return "Nimi: " + Merkki + ", Malli: " + Tyyppi + ", Rengas: " + Rengaskoko;
+        }
+    }
+
+    class Auto
+    {
+        public string Nimi { get; set; }
+        public string Malli { get; set; }
+
+        private const int maxRenkaidenLkm = 4;
+        private int renkaidenLkm = 0;
+
+        public List<Rengas> Renkaat { get; }
+
+        public Auto()
+        {
+            Renkaat = new List<Rengas>();
+        }
+        public void LisaaRengas(Rengas rengas)
+        {
+            if (renkaidenLkm < maxRenkaidenLkm)
+            {
+                Renkaat.Add(rengas);
+                renkaidenLkm++;
+                Console.WriteLine("Rengas {0} lisätty ajoneuvoon {1}", rengas.Merkki, Nimi);
+            }
+            else
+            {
+                Console.WriteLine("Ei enää renkaita, kiitos!");
+            }
+        }
+        public override string ToString()
+        {
+            string tulosta = "\nAutossa " + Nimi + " malli: " + Malli + ", renkaat:";
+            foreach (Rengas r in Renkaat)
+            {
+                if (r != null) tulosta += "\n- " + r.ToString();
+            }
+            return tulosta;
+        }
+    }
+}
