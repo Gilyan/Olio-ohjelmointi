ba4197001840db16b214e2400093f0d4bd2222b3
k8517 <k8517@ICT-421-WS-05>
2017-01-23 10:54:19 +0200

    Uusi harjoitus lisätty

---
+++ Tuntiharjoitukset3/Harjoitus1.cs
@@ -0,0 +1,17 @@
+﻿/* **************************************
+Harjoitellaan dfhfd käyttöä.
+
+Luotu 23.1.2017
+
+Minttu Mäkäläinen K8517 @ JAMK
+************************************** */
+
+using System;
+
+namespace JAMK.IT
+{
+    public class Harjoitus1
+    {
+       
+    }
+}
---
+++ Tuntiharjoitukset3/Program.cs
@@ -0,0 +1,61 @@
+﻿/* **************************************
+Tuntiharjoituksille pääohjelma.
+
+Luotu 23.1.2017
+
+Minttu Mäkäläinen K8517 @ JAMK
+************************************** */
+
+using JAMK.IT;                          // Helpottaa meidän luokkien löytämistä
+using System;
+
+namespace Tuntiharjoitukset
+{
+    class Harjoitukset
+    {
+        static void Main(string[] args)
+        {
+            //TestaaHissi();            // Tehtävä 1
+
+        }
+
+        /**********************************************
+        *      Tehtävä 1 - Hissi-olion testausta      *
+        **********************************************/
+        static void TestaaHissi()
+        {
+
+        }
+
+        /**********************************************
+        *    Tehtävä 2 - Vahvistin-olion testausta    *
+        **********************************************/
+
+
+        /**********************************************
+        *  Tehtävä 3 - Henkilotiedot-olion testausta  *
+        **********************************************/
+
+
+        /**********************************************
+        *   Tehtävä 4 - Kulkuneuvot-olion testausta   *
+        **********************************************/
+
+
+        /**********************************************
+         *     Tehtävä 5 - Radio-olion testausta      *
+        **********************************************/
+
+
+        /**********************************************
+         *   Tehtävä 6 - Kirjahylly-olion testausta   *
+        **********************************************/
+
+
+        /**********************************************
+         *  Tehtävä 7 - Paivakirja-olion testausta    *
+        **********************************************/
+
+
+    }
+}
